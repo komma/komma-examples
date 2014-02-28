29dca73f46318f78b535896824e752e52dd3a758
Heiner Reinhardt <heiner.reinhardt@googlemail.com>
2014-02-28 15:18:46 +0100

    Extended tutorial by inheritance and resulting RDF serialization.
    Change-Id: I96aa37f20e64165f8f9ab236171bef75afd37a06

---
+++ net.enilink.komma.example.objectmapping/src/main/java/net/enilink/komma/example/objectmapping/model/Magazine.java
@@ -0,0 +1,12 @@
+package net.enilink.komma.example.objectmapping.model;
+
+import net.enilink.composition.annotations.Iri;
+
+@Iri(Library.URI_STRING + "Magazine")
+public interface Magazine extends Document {
+
+	/*
+	 * Some magazine specific attributes
+	 */
+
+}
